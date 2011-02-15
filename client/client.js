2bc25040840c1dea4bd59e13153bb266ed46115f
William Becker <wbekcer@gmail.com>
2011-02-15 20:01:22 +0000

    tidy up the styling

--- client/client.js
+++ client/client.js
@@ -1,11 +1,19 @@
+/*jslint white: true, onevar: true, undef: true, newcap: true, nomen: false, regexp: true, plusplus: true, bitwise: true, browser: true, maxerr: 5, maxlen: 80, indent: 2 */
+/*global io, console*/
 var socket = new io.Socket();
 socket.connect();
 socket.on('connect', function () { 
-})
-socket.on('message', function(message) { 
-  document.getElementById("out").value+=message+"\n";
-})
-socket.on('disconnect', function(){ console.debug("disconnected", arguments); })
+});
+socket.on('message', function (message) { 
+  var parent = document.getElementById("out"),
+      holder = document.createElement("div");
+  holder.className = "message";
+  holder.textContent = message;
+  parent.appendChild(holder);
+});
+socket.on('disconnect', function () { 
+  console.debug("disconnected", arguments); 
+});
 
 document.getElementById('send').addEventListener('click', 
   function () {
