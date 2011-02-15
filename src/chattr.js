d2957f49e74298e586fe391166bab8cb5923bbf4
William Becker <wbekcer@gmail.com>
2011-02-15 20:01:05 +0000

    send all static files from the client folder, but from the base server client.htm

--- src/chattr.js
+++ src/chattr.js
@@ -1,12 +1,20 @@
+/*jslint white: true, onevar: true, undef: true, newcap: true, nomen: false, regexp: true, plusplus: true, bitwise: true, maxerr: 5, maxlen: 80, indent: 2 */
+/*global require */
+
 (function () {
   var http = require('http'), 
       io = require('../../socket.io-node'),
       fs = require('fs'),
       util = require('util'),
       server, socket, clients;
-  server = http.createServer(function(req, res){
-    fs.readFile('client/client.htm', "binary", function (err, file) {
-       if (!err) {
+  server = http.createServer(function (req, res) {
+    util.log(req.url);
+    var url = req.url;
+    if (url === "/") {
+      url += "client.htm";
+    }
+    fs.readFile('client' + url, "binary", function (err, file) {
+      if (!err) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(file, 'binary');
       }
@@ -19,25 +27,27 @@
   });
 
   server.listen(8000);
+  socket = io.listen(server);
 
-  var socket = io.listen(server);
+  clients = {};
 
-  var clients = {}
-  socket.on('connection', function(client){
+  socket.on('connection', function (client) {
     clients[client.sessionId] = client;
     util.log('connected');
     
-    client.on('message', function(message){ 
-      util.log('message: '+message); 
-      for (var c in clients) {
-        if (c !== client.sessionId) {
-          clients[c].send(message);
+    client.on('message', function (message) { 
+      var sessionId;
+      util.log('message: ' + message); 
+      for (sessionId in clients) {
+        if (clients.hasOwnProperty(sessionId)) {
+          clients[sessionId].send(message);
         }
       }
-    })
-    client.on('disconnect', function(){ 
+    });
+
+    client.on('disconnect', function () { 
       util.log('disconnected'); 
       delete clients[client.sessionId];
-    })
+    });
   });
 }());
