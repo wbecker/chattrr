9f55cae6b8a402873a56af3a6f69732c20d3dc15
wbecker <wbecker@gmail.com>
2011-02-22 19:41:07 +0000

    Update license

--- client/client.js
+++ client/client.js
@@ -1,3 +1,22 @@
+/*
+    Copyright 2011 William Becker
+
+    This file is part of Chattrr.
+
+    Chattrr is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Chattrr is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Chattrr.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*jslint white: true, onevar: true, undef: true, newcap: true, nomen: false, regexp: true, plusplus: true, bitwise: true, browser: true, maxerr: 5, maxlen: 80, indent: 2 */
 /*global _, window, io, console*/
 (function () {
