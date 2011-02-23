b0e694c378d30bd3850821fb5eaec40372272b65
wbecker <wbecker@gmail.com>
2011-02-23 11:12:00 +0000

    use the location, not the document to get the url

--- client/client.js
+++ client/client.js
@@ -170,7 +170,7 @@
         clearInterval(retryTimeout);
       }
       var connectMessage = {};
-      connectMessage.url = document.host + document.pathname;
+      connectMessage.url = document.location.host + document.location.pathname;
       connectMessage.userToken = userToken;
       socket.send(JSON.stringify(connectMessage));
       _(lostMessages).keys().sort().forEach(function (key) {
